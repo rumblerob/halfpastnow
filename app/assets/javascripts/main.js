d2941cf2e7bd88303bcba4d30b14ece9608d5ed3
Rob Rumble <rumblerob@gmail.com>
2012-02-01 15:03:41 -0600

    skeleton for events/venue mode

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -23,4 +23,27 @@ $(function() {
 	});
 	
 	$('#content .sidebar .inner .filter.date .date ').datepicker();
+	
+	$('#content .main .events li').click(function() {
+		modal({type: "event", id: $(this).attr("event_id")});
+	});
+	
+	$('#content .main .events li .venue').click(function(event) {
+		event.stopPropagation();
+		modal({ type: "venue", id: 1});
+	});
+	
+	$('.mode .overlay').click(function() {
+			$(this).parent().hide();
+			history.pushState({}, "main mode", "/events");
+	});
 });
+
+function modal(thing) {
+	var offset = { width: 60, height: 60 };
+	history.pushState(thing, thing.type + " mode", "/events?" + thing.type + "_id=" + thing.id);
+	$('.mode.' + thing.type + ' .window').width($(window).width() - offset.width * 2);
+	$('.mode.' + thing.type + ' .window').height($(window).height() - offset.height * 2);
+	$('.mode.' + thing.type + ' .window').css({ left: offset.width, top: offset.height });
+	$('.mode.' + thing.type).show();
+}
