17c9f87b1bcc309d1dd7b72682f9aec56892e343
Michael Wei <michael.y.wei@gmail.com>
2012-03-01 22:00:36 -0600

    thinking sphinx search

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.3'
 
 gem 'pg'
 gem 'thin'
+gem 'thinking-sphinx', '2.0.11'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ActiveRecord::Base
   belongs_to :venue
   has_many :occurrences, :dependent => :destroy
   accepts_nested_attributes_for :occurrences, :allow_destroy => true
+
+  
+  define_index do
+        indexes title, :sortable => true
+        indexes description
+        indexes venue.name
+  end
+
+    
 end
