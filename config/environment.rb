b69fb6ff465d9f6b86419a13569ded64de0bb878
Michael Wei <michael.y.wei@gmail.com>
2012-03-07 21:12:17 -0600

    flying sphinx

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.gem 'thinking-sphinx',
+  :lib     => 'thinking_sphinx',
+  :version => '2.0.10'
+
 config.gem 'flying-sphinx',
   :version => '0.6.1'
 
