36dc904380f20cf7b8cc9455a6d3ca2a7ea8d62c
Michael Wei <michael.y.wei@gmail.com>
2012-03-19 21:01:37 -0500

    Modified asset precompiling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229164941) do
+ActiveRecord::Schema.define(:version => 20120318203155) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(:version => 20120229164941) do
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phonenumber"
   end
 
 end
